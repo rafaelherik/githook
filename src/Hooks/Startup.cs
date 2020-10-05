ded10ed6244e30e27ed8a26769d571723b1c6b38
Rafael <rafaelherik@gmail.com>
2020-10-05 13:58:00 +0100

    Update Startup.cs

--- src/Hooks/Startup.cs
+++ src/Hooks/Startup.cs
@@ -28,7 +28,6 @@ namespace Hooks
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
